fa80d25e0e0cb04a3e9d80b8749024d8ad0881ff
pranithaiwiz <148210671+pranithaiwiz@users.noreply.github.com>
2024-08-08 00:59:57 +0530

    2nd commit.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CreateTodo from './components/CreateTodo'
+import Todo from './components/Todo'
 
 function App() {
 
   return (
     <>
-      hi there 
+       <CreateTodo/>
+       <Todo/>
     </>
   )
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div>
+        <h1>Title</h1>
+        <h2>Description</h2>
+        <button>COMPLETED</button>
+    </div>
+  )
+}
+
+export default Todo
